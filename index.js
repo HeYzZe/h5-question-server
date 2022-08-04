a0eb15a18b55669cd235eddd231632a44778f89f
heyz <1598217144@qq.com>
2022-08-04 12:00:16 +0800

    fix: 查询接口参数

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
 //首先加载express
 const express = require("express")
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { http } = require('./service')
 const { getSign } = require('./utils')
 
 const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
 //端口号
 const port = 80
 
 //这里仅列举发送GET请求
-app.get("/get", (req, res) => {
-  const query = {
-    mchId: '1658999813335',
-    orderNo: '1659428896008',
+app.post("/getPayInfo", (req, res) => {
+  if (req.body.mchId && req.body.orderNo) {
+    const query = {
+      mchId: req.body.mchId,
+      orderNo: req.body.orderNo,
+    }
+    const sign = getSign(query)
+    let urlencoded = new URLSearchParams();
+    Object.entries({
+      ...query,
+      sign
+    }).forEach((ele) => {
+      urlencoded.append(ele[0], ele[1].toString())
+    })
+    http('qpayorder', {
+      body: urlencoded
+    }).then((response) => {
+      console.log(response, new Date().toLocaleString())
+      res.send(response)
+    })
+  } else {
+    throw new Error('参数错误')
   }
-  const sign = getSign(query)
-  let urlencoded = new URLSearchParams();
-  Object.entries({
-    ...query,
-    sign
-  }).forEach((ele) => {
-    urlencoded.append(ele[0], ele[1].toString())
-  })
-  http('qpayorder', {
-    body: urlencoded
-  }).then((response) => {
-    console.log(response, new Date().toLocaleString())
-    res.send(response)
-  })
 })
+
+function errorHandler(err, req, res, next) {
+  res.status(501).json({ error: -1, msg: err.message })
+}
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log("server is start,port is", port, new Date().toLocaleString()))
